5fc45702161f5741a7de7526a7aa1bf51542e207
China-hanwudi <692919993@qq.com>
2026-08-09 20:32:01 +0800

    Revert "更新 CARMA-Affect 当前方案、进度与冻结实验结果"

--- experiment/src/hva_affect/__init__.py
+++
@@ -1,5 +0,0 @@
-"""HVA-Affect research code."""
-
-from .data_contract import ContractError, audit_meld, build_same_speaker_history
-
-__all__ = ["ContractError", "audit_meld", "build_same_speaker_history"]
